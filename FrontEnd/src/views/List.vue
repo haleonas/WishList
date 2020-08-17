28dad6415c6c17d4e065e313bc8208b72a5fae4b
haleonas <altes@live.se>
2020-08-17 11:54:59 +0200

    added a list component for either create a list or edit a current list

---
+++ FrontEnd/src/views/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "List",
+  beforeMount() {
+    this.checkList()
+  },
+  methods: {
+    async checkList() {
+      await axios.get('')
+      if (this.$route.params.list) {
+        console.log('this route had a parameter')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
