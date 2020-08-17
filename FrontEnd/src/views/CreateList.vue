a2123c7892ca28c82aaef2496b30192a177566cb
haleonas <altes@live.se>
2020-08-17 12:31:56 +0200

    added item form creation

--- FrontEnd/src/views/CreateList.vue
+++ FrontEnd/src/views/CreateList.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-
+      <div v-for="(item,index) in listItems" :key="index">
+        <app-hej-varlden :item="item" :remove="remove"></app-hej-varlden>
+      </div>
+      <button class="button is-primary is-medium"
+              @click="itemForm">
+        Add item to wishlist
+      </button>
     <button class="button is-medium" @click="createList"></button>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import AddItemForm from "@/components/AddItemForm";
 
 export default {
   name: "List",
@@ -27,8 +34,12 @@ export default {
       if (this.$route.params.list) {
         console.log('this route had a parameter')
       }
+    },
+    itemForm(){
+
     }
-  }
+  },
+
 }
 </script>
 
